0899d3f24bc8fc1a1d3482ce0536522bdb700b04
Mark Johnson <marklj@gmail.com>
2012-03-15 22:24:16 -0700

    added classes

---
+++ src/com/bbmcsolutions/ecometrics/data.java
@@ -0,0 +1,21 @@
+package com.bbmcsolutions.ecometrics;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class data extends Activity {
+    /** Called when the activity is first created. */
+	
+	TextView t;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
+        
+        t = (TextView) findViewById(R.id.contactsText);
+        
+    }
+}
