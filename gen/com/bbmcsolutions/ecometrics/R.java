40dd695d92cca41fe319d2a98babe137575b868f
Mark Johnson <marklj@gmail.com>
2012-03-16 00:24:40 -0700

    rounded buttons

--- gen/com/bbmcsolutions/ecometrics/R.java
+++ gen/com/bbmcsolutions/ecometrics/R.java
@@ -28,10 +28,12 @@ public final class R {
         public static final int urlButt=0x7f050004;
     }
     public static final class layout {
-        public static final int config=0x7f030000;
-        public static final int contacts=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int metrics=0x7f030003;
+        public static final int bluebuttonshape=0x7f030000;
+        public static final int buttonshape=0x7f030001;
+        public static final int config=0x7f030002;
+        public static final int contacts=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int metrics=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
