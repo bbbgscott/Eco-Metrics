7d3b23b47885086d9def0c5fa7096d498d893d73
Mark Johnson <marklj@gmail.com>
2012-03-15 22:10:29 -0700

    Main page with buttons

--- gen/com/bbmcsolutions/ecometrics/R.java
+++ gen/com/bbmcsolutions/ecometrics/R.java
@@ -15,10 +15,11 @@ public final class R {
         public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f050001;
-        public static final int button2=0x7f050002;
+        public static final int configButt=0x7f050001;
+        public static final int contactButt=0x7f050003;
         public static final int imageView1=0x7f050000;
-        public static final int textView1=0x7f050003;
+        public static final int metricsButt=0x7f050002;
+        public static final int textView1=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
