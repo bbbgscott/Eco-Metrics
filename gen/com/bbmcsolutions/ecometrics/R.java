9165a37985d4d986ae14d8cd7fd68c171609dc14
Mark Johnson <marklj@gmail.com>
2012-03-15 21:09:37 -0700

    updated main view

--- gen/com/bbmcsolutions/ecometrics/R.java
+++ gen/com/bbmcsolutions/ecometrics/R.java
@@ -15,13 +15,17 @@ public final class R {
         public static final int logo=0x7f020001;
     }
     public static final class id {
+        public static final int button1=0x7f050001;
+        public static final int button2=0x7f050002;
         public static final int imageView1=0x7f050000;
+        public static final int textView1=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int copyright=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
