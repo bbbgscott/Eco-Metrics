51a7a04f96da382b63ac1ac03e959e016abc1875
Mark Johnson <marklj@gmail.com>
2012-03-15 20:28:14 -0700

    added image to main file

--- src/com/bbmcsolutions/ecometrics/main.java
+++ src/com/bbmcsolutions/ecometrics/main.java
@@ -2,12 +2,20 @@ package com.bbmcsolutions.ecometrics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
+	
+	ImageView image;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        image = (ImageView) findViewById(R.id.imageView1);
+        
+        
     }
 }
